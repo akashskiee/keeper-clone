e4d9ad1687452a1bbc546903962397c063f631d7
Akash Kumar <akashpk1997@gmail.com>
2020-04-05 00:00:51 +0530

    Modified delete button

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -6,7 +6,7 @@ function InputArea() {
       <form>
         <input name="title" placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <button>+</button>
       </form>
     </div>
   );
