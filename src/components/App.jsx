1359853165dd82e3d405b13b1ef884a046dd2998
Akash Kumar <akashpk1997@gmail.com>
2020-04-05 15:41:39 +0530

    Added add keep and delete keep functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes"
 import InptArea from "./InputArea"
 
-function addNotes(noteItem) {
-    return(
-        <Note
-            key={noteItem.id}
-            title={noteItem.title}
-            content={noteItem.content.substring(0, 50)}
-         />
-    )
-}
+
 
 function App(){
+    const [notes, addKeepNotes] = useState([]);
+    function addNote(keep){
+        addKeepNotes(prevValue => {
+            return [...prevValue, keep]
+        }) 
+    }
+function deleteNotes(id){
+    addKeepNotes(prevValue => {
+        return prevValue.filter((item, index) => {
+            return index !== id;
+        })
+    })
+}
     return(
         <div>
             <Header />
-            <InptArea />
-            {notes.map(addNotes)}
+            <InptArea
+            onAdd={addNote} />
+            {notes.map((note, index) => {
+                return <Note
+                title={note.title}
+                key={index}
+                id={index}
+                content={note.content}
+                onDelete={deleteNotes} />
+            })}
+            
             <Footer />
         </div>
     )
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
+function InputArea(props) {
+  const [keeperText, getText] = useState({
+    title: "",
+    content: ""
+  });
+
+  function getKeeper(event){
+    const {name, value}  = event.target;
+    getText(prevValue => {
+        return {
+          ...prevValue,
+          [name]: value
+        }
+    })
+  }
+  function addText(event){
+    props.onAdd(keeperText);
+    getText("");
+    event.preventDefault();
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>+</button>
+        <input onChange={getKeeper} value={keeperText.title} name="title" required autoComplete="off"  placeholder="Title" />
+        <textarea onChange={getKeeper} value={keeperText.content} required name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={addText}>+</button>
       </form>
     </div>
   );
